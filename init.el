092e63e7a435a770807e9f95f6fbc88f89133892
Miika Nissi <miika@miikanissi.com>
2024-10-30 16:52:02 +0300

    meow consult keymap

--- init.el
+++ init.el
@@ -141,7 +141,13 @@ Usage:
        (global-set-key (kbd ,prefix) ',keymap-alias)
        ,@keymap-bindings)))
 
-
+(my-defkeymap
+   "find" "C-c f"
+   '("r" . consult-recent-file)
+   '("f" . consult-fd)
+   '("g" . consult-ripgrep)
+   '("l" . consult-line)
+   '("i" . consult-imenu))
 
 (use-package emacs
   :custom
@@ -402,34 +408,34 @@ Usage:
   :config
   ;; Optionally configure preview. The default value
   ;; is 'any, such that any key triggers the preview.
-  ;; (setq consult-preview-key 'any)
-  ;; (setq consult-preview-key "M-.")
-  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  (setq consult-preview-key 'any)
+  (setq consult-preview-key "M-.")
+  (setq consult-preview-key '("S-<down>" "S-<up>"))
 
   ;; For some commands and buffer sources it is useful to configure the
   ;; :preview-key on a per-command basis using the `consult-customize' macro.
-  ;; (consult-customize
-  ;; consult-theme :preview-key '(:debounce 0.2 any)
-  ;; consult-ripgrep consult-git-grep consult-grep
-  ;; consult-bookmark consult-recent-file consult-xref
-  ;; consult--source-bookmark consult--source-file-register
-  ;; consult--source-recent-file consult--source-project-recent-file
-  ;; :preview-key "M-."
-  ;; :preview-key '(:debounce 0.4 any))
+  (consult-customize
+  consult-theme :preview-key '(:debounce 0.2 any)
+  consult-ripgrep consult-git-grep consult-grep
+  consult-bookmark consult-recent-file consult-xref
+  consult--source-bookmark consult--source-file-register
+  consult--source-recent-file consult--source-project-recent-file
+  :preview-key "M-."
+  :preview-key '(:debounce 0.4 any))
 
   ;; By default `consult-project-function' uses `project-root' from project.el.
   ;; Optionally configure a different project root function.
    ;;;; 1. project.el (the default)
-  ;; (setq consult-project-function #'consult--default-project--function)
+  (setq consult-project-function #'consult--default-project--function)
    ;;;; 2. vc.el (vc-root-dir)
-  ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
+  (setq consult-project-function (lambda (_) (vc-root-dir)))
    ;;;; 3. locate-dominating-file
-  ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
+  (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
    ;;;; 4. projectile.el (projectile-project-root)
   (autoload 'projectile-project-root "projectile")
   (setq consult-project-function (lambda (_) (projectile-project-root)))
    ;;;; 5. No project support
-  ;; (setq consult-project-function nil)
+   (setq consult-project-function nil)
   )
 
 (use-package diminish)
